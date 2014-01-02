6c1e4dc7a338b36561c432904726a77193cdfce8
Eric Swanson <email@iseric.com>
2014-01-02 13:34:40 -0700

    add comments

--- VS/src/MVC/App.fs
+++ VS/src/MVC/App.fs
@@ -15,107 +15,168 @@ open System.Web
 open System.Web.Mvc
 open System.Web.Routing
 
+/// Contract for application session events
 type IAppSessionEvents = 
+    /// Called when a session starts
     abstract OnSessionStart : unit -> IAppSessionEvents
+    /// Called when a session ends
     abstract OnSessionEnd : unit -> IAppSessionEvents
-
+    
+/// Contract for application request events
 type IAppRequestEvents = 
+    /// Called when a request starts
     abstract OnBeginRequest : unit -> IAppRequestEvents
+    /// Called when authentication is requested
     abstract OnAuthenticateRequest : unit -> IAppRequestEvents
-
+    
+/// Contract for application events
 type IAppEvents = 
+    /// Called when the application starts
     abstract OnStart : unit -> IAppEvents
+    /// Called when the application encounters an error
     abstract OnError : unit -> IAppEvents
+    /// Called when the application ends
     abstract OnEnd : unit -> IAppEvents
-
+    
+/// Contract for an MVC application
 type IApp = 
     inherit IAppSessionEvents
     inherit IAppRequestEvents
     inherit IAppEvents
 
+/// Contract for initializing MVC Areas
 type IAreaInitialization =
+    /// Register MVC Areas
     abstract RegisterAreas : unit -> IAreaInitialization
 
+/// Default implementation for initializing MVC Areas
 type DefaultAreaInitialization() =
+    /// Contract for initializing MVC Areas
     abstract RegisterAreas : unit -> IAreaInitialization
     default this.RegisterAreas() =
         AreaRegistration.RegisterAllAreas()
         this :> IAreaInitialization
-
+        
+    // contract implementation
     interface IAreaInitialization with
         member this.RegisterAreas() = this.RegisterAreas()
-
+        
+/// Contract for initializing MVC Filters
 type IFilterInitialization =
+    /// Register MVC Filters
     abstract RegisterFilters : unit -> IFilterInitialization
-
+    
+/// Default implementation for initializing MVC Filters
 type DefaultFilterInitialization() =
+    /// Register MVC Filters
     abstract RegisterFilters : unit -> IFilterInitialization
     default this.RegisterFilters() =
         GlobalFilters.Filters.Add(HandleErrorAttribute())
         this :> IFilterInitialization
-
+        
+    // contract implementation
     interface IFilterInitialization with
         member this.RegisterFilters() = this.RegisterFilters()
-
+        
+/// Contract for initializing MVC Routes
 type IRouteInitialization =
+    /// Register MVC Routes
     abstract RegisterRoutes : unit -> IRouteInitialization
     
+/// Default record type for a Controller Route
 type ControllerRouteDefaults =
     {
+        /// The name of the default controller (without the "Controller" name suffix)
         controller: string
+        /// The name of the default controller action
         action: string
-        id: UrlParameter
     }
 
+/// A single-page application controller
 type SpaController() =
     inherit Controller()
     
+    /// The index action for the SPA
     member this.Index() =
         this.View("~/SPA.cshtml")
-
+        
+/// Default implementation for initializing MVC Routes
 type DefaultRouteInitialization() =
+    /// Register MVC Routes
     abstract RegisterRoutes : unit -> IRouteInitialization
     default this.RegisterRoutes() =
         let ns = this.GetType().Namespace
-        RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
-        ignore <| RouteTable.Routes.MapRoute("Default", "{controller}/{action}", box { controller = "Spa"; action = "Index"; id = UrlParameter.Optional }, [|ns|])
-        this :> IRouteInitialization
 
+        // ignore .axd requests
+        RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
+
+        // map the default SPA route
+        ignore <| RouteTable.Routes.MapRoute("Default", "{controller}/{action}", box { controller = "Spa"; action = "Index" }, [|ns|])
+
+        this :> IRouteInitialization
+        
+    // contract implementation
     interface IRouteInitialization with
         member this.RegisterRoutes() = this.RegisterRoutes()
 
+/// Contract for application startup
 type IAppStartup =
+    /// Starts the application
     abstract Start : IApp -> IApp
-
+    
+/// Default implementation for application startup
 type DefaultAppStartup(logInit:Log.ILogInitializer, areasInit:IAreaInitialization, filtersInit:IFilterInitialization, routesInit:IRouteInitialization) =
+    /// Starts the application
     abstract Start : IApp -> IApp
     default this.Start(app) =
+        // init logging
         let log = logInit.Initialize()
-        log.Startup()
-        ignore <| areasInit.RegisterAreas()
-        ignore <| filtersInit.RegisterFilters()
-        ignore <| routesInit.RegisterRoutes()
-        app
 
+        // log the app startup event
+        log.Startup()
+
+        // init areas
+        ignore <| areasInit.RegisterAreas()
+        
+        // init filters
+        ignore <| filtersInit.RegisterFilters()
+        
+        // init routes
+        ignore <| routesInit.RegisterRoutes()
+
+        app
+        
+    // contract implementation
     interface IAppStartup with
         member this.Start(app) = this.Start(app)
-
+        
+/// Contract for application shutdown
 type IAppShutdown =
+    /// Shuts down the application
     abstract Shutdown : IApp -> IApp
-
+    
+/// Default implementation for application startup
 type DefaultAppShutdown(logDispose:Log.ILogDisposer, log:Log.ILog) =
+    /// Shuts down the application
     abstract Shutdown : IApp -> IApp
     default this.Shutdown(app) =
-        logDispose.Dispose(log)
+        // log the app shutdown event
         log.Shutdown()
-        app
 
+        // clean up logging
+        logDispose.Dispose(log)
+        
+        app
+        
+    // contract implementation
     interface IAppShutdown with
         member this.Shutdown(app) = this.Shutdown(app)
 
+/// SkyJinx Web Application
 type App(startup:IAppStartup, shutdown:IAppShutdown) = 
     inherit HttpApplication()
     
+    /// Default constructor builds default start-up and shut-down implementation
     new() = 
         let log = Log.GetLog()
         let logInit = Log.DefaultLogInitializer(log)
@@ -127,44 +188,59 @@ type App(startup:IAppStartup, shutdown:IAppShutdown) =
         let startup = DefaultAppStartup(logInit, areaInit, filterInit, routeInit)
         let shutdown = DefaultAppShutdown(logDispose, log)
         new App(startup, shutdown)
-
+        
+    /// Called when the application starts
     abstract OnStart : unit -> IApp
     default this.OnStart() = 
         startup.Start(this)
+    /// ASP.NET default application start method for an HttpApplication
     member this.Application_Start(sender:obj, e:EventArgs) =
         ignore <| this.OnStart()
     
+    /// Called when a session starts
     abstract OnSessionStart : unit -> IApp
     default this.OnSessionStart() = this :> IApp
+    /// ASP.NET default session start method for an HttpApplication
     member this.Session_Start(sender:obj, e:EventArgs) = 
         ignore <| this.OnSessionStart()
     
+    /// Called when a request begins
     abstract OnBeginRequest : unit -> IApp
     default this.OnBeginRequest() = this :> IApp
+    /// ASP.NET default begin request method for an HttpApplication
     member this.Application_BeginRequest(sender:obj, e:EventArgs) =
         ignore <| this.OnBeginRequest()
     
+    /// Called when authentication is requested
     abstract OnAuthenticateRequest : unit -> IApp
     default this.OnAuthenticateRequest() = this :> IApp
+    /// ASP.NET default authenticate request method for an HttpApplication
     member this.Application_AuthenticateRequest(sender:obj, e:EventArgs) =
         ignore <| this.OnAuthenticateRequest()
     
+    /// Called when an application error occurs
     abstract OnError : unit -> IApp
     default this.OnError() = this :> IApp
+    /// ASP.NET default application error method for an HttpApplication
     member this.Application_Error(sender:obj, e:EventArgs) =
         ignore <| this.OnError()
     
+    /// Called when a session ends
     abstract OnSessionEnd : unit -> IApp
     default this.OnSessionEnd() = this :> IApp
+    /// ASP.NET default session end method for an HttpApplication
     member this.Session_End(sender:obj, e:EventArgs) = 
         ignore <| this.OnSessionEnd()
     
+    /// Called when the application ends
     abstract OnEnd : unit -> IApp
     default this.OnEnd() = 
         shutdown.Shutdown(this)
+    /// ASP.NET default application end method for an HttpApplication
     member this.Application_End(sender:obj, e:EventArgs) = 
         ignore <| this.OnEnd()
     
+    // contract implementation
     interface IApp with
         member this.OnStart() = this.OnStart() :> IAppEvents
         member this.OnSessionStart() = this.OnSessionStart() :> IAppSessionEvents
@@ -178,11 +254,15 @@ open System.Runtime.CompilerServices
 
 [<Extension>]
 module UrlExtensions =
+    /// Returns a URL string specific to a path used by RequireJS
     [<Extension>]
     let Require(url:UrlHelper, vpath:string) =
         match url with
         | null -> MvcHtmlString.Empty
         | _ ->
+            // get the regular URL
             let path = url.Content(vpath)
+            // remove .js
             let requirejsPath = path.Replace(".js", "")
+            // return
             MvcHtmlString.Create(requirejsPath)
--- VS/src/MVC/Log.fs
+++ VS/src/MVC/Log.fs
@@ -15,57 +15,75 @@ open System.Diagnostics
 open System.Diagnostics.Tracing
 open Microsoft.Practices.EnterpriseLibrary.SemanticLogging
 
+/// Contract for MVC Logging
 type ILog =
+    /// Logs the application startup event
     abstract Startup : unit -> unit
+    /// Logs when areas are registered
     abstract RegisterAreas : unit -> unit
+    /// Logs when a filter is registered
     abstract RegisterFilter : string -> unit
+    /// Logs when a route is registered
     abstract RegisterRoute : string * string -> unit
+    /// Logs the application shutdown event
     abstract Shutdown : unit -> unit
 
+/// Contract for initializing logging
 type ILogInitializer =
+    /// Initializes the logging implementation
     abstract Initialize : unit -> ILog
 
+/// Contract for disposing of logging
 type ILogDisposer =
+    /// Disposes of the logging implementation
     abstract Dispose : ILog -> unit
 
+/// The default MVC logging event source.
 [<EventSource(Name = "SkyJinx-Web-MVC")>]
 type MvcEventSource private() =
     inherit EventSource()
 
     static let log = new Lazy<_>(fun _ -> new MvcEventSource() :> ILog)
 
+    /// Gets the static instance of the logging implementation
     static member Instance with get() = log.Value
 
+    /// Logs the application startup event
     [<Event(1, Message = "Starting up.", Level = EventLevel.Informational)>]
     member this.Startup() =
         Debug.WriteLine("Starting up.")
         if (this.IsEnabled()) then
             this.WriteEvent(1)
-
+            
+    /// Logs when areas are registered
     [<Event(50, Message = "Registering MVC Areas.", Level = EventLevel.Informational)>]
     member this.RegisterAreas() =
         Debug.WriteLine("Registering MVC Areas.")
         if (this.IsEnabled()) then
             this.WriteEvent(50)
-
+            
+    /// Logs when a filter is registered
     [<Event(60, Message = "Registered MVC Filter: {0}", Level = EventLevel.Informational)>]
     member this.RegisterFilter(name:string) =
         Debug.WriteLine(sprintf "Registered MVC Filter: %s" name)
         if (this.IsEnabled()) then
             this.WriteEvent(60, name)
-
+            
+    /// Logs when a route is registered
     [<Event(70, Message = "Registered MVC Route \"{0}\": {1}", Level = EventLevel.Informational)>]
     member this.RegisterRoute(name:string, path:string) =
         Debug.WriteLine(sprintf "Registered MVC Route \"%s\": %s" name path)
         if (this.IsEnabled()) then
             this.WriteEvent(70, name, path)
-
+            
+    /// Logs the application shutdown event
     [<Event(9999, Message = "Shutting down.", Level = EventLevel.Informational)>]
     member this.Shutdown() =
         Debug.WriteLine("Shutting down.")
         if (this.IsEnabled()) then
             this.WriteEvent(9999)
 
+    // contract implementation
     interface ILog with
         member this.Startup() = this.Startup()
         member this.RegisterAreas() = this.RegisterAreas()
@@ -73,13 +91,17 @@ type MvcEventSource private() =
         member this.RegisterRoute(name, path) = this.RegisterRoute(name, path)
         member this.Shutdown() = this.Shutdown()
 
+/// Provides the default implementation of MVC logging
 let GetLog() = MvcEventSource.Instance
 
+/// The default log initializer
 type DefaultLogInitializer(log:ILog) =
     let listener = new ObservableEventListener()
     
+    /// Initializes the logging implementation
     member this.Initialize() =
         
+        // if the logging implementation is an 'EventSource', setup
         match log with
         | :? EventSource as src -> 
             listener.EnableEvents(src, EventLevel.LogAlways, Keywords.All)
@@ -87,15 +109,19 @@ type DefaultLogInitializer(log:ILog) =
         | _ -> ()
 
         log
-
+        
+    // contract implementation
     interface ILogInitializer with
         member this.Initialize() = this.Initialize()
-
+        
+/// The default log disposer
 type DefaultLogDisposer() =
     let listener = new ObservableEventListener()
     
+    /// Disposes of the logging implementation
     member this.Dispose(log:ILog) =
         
+        // if the logging implementation is an 'EventSource', cleanup
         match log with
         | :? EventSource as src -> 
             listener.DisableEvents(src)
@@ -103,6 +129,7 @@ type DefaultLogDisposer() =
         | _ -> ()
 
         ()
-
+        
+    // contract implementation
     interface ILogDisposer with
         member this.Dispose(log) = this.Dispose(log)
