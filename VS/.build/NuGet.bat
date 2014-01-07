e7ed666d1beef548e63ce7b00ae3a91a9b2fd5c5
Eric Swanson <email@iseric.com>
2014-01-06 17:37:19 -0700

    fix nuget bat file

--- VS/.build/NuGet.bat
+++ VS/.build/NuGet.bat
@@ -2,4 +2,4 @@ if not exist ..\.publish\ mkdir ..\.publish\
 
 ..\.nuget\nuget.exe pack ..\src\MVC\MVC.fsproj -OutputDirectory ..\.publish\
 
-..\.nuget\nuget.exe pack ..\src\Web.Angular.Bootstrap2\Web.Angular.Bootstrap2.csproj -Exclude **\*.dll -OutputDirectory ..\.publish\
+..\.nuget\nuget.exe pack ..\src\Web.Angular.Bootstrap\Web.Angular.Bootstrap.csproj -Exclude **\*.dll -OutputDirectory ..\.publish\
